8be708d8299d4881f9c5af47b9d937bdab94f7be
Lucille Amanda Jureidini <145380670+cdblast@users.noreply.github.com>
2023-11-08 11:14:08 -0800

    Colliders + Collectibles update
    World Interactions and Colliders added
    previous update was fixed
    ruby now correctly collides w boxes and water !!!!!!

--- RubysAdvJureidini/Assets/Scripts/RubyController.cs
+++ RubysAdvJureidini/Assets/Scripts/RubyController.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    public float speed = 3.0f;
+
+    public int maxHealth = 5;
+
+    int currentHealth;
+    public int health { get { return currentHealth; } }
+
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
 
     // Start is called before the first frame update
     void Start()
     {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+
+        currentHealth = maxHealth;
         //QualitySettings.vSyncCount = 0;
         //Application.targetFrameRate = 10;
     }
@@ -15,13 +28,22 @@ public class RubyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
 
-        Vector2 position = transform.position;
-        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
         position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;      
         
-        transform.position = position;
+        rigidbody2d.MovePosition(position);
+    }
+    
+    public void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
